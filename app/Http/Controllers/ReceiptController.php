5d480b0588b328e1c59c5b174e718d7b77d343bd
zazazach-my <78088798+zazazach-my@users.noreply.github.com>
2022-11-22 21:38:47 +0800

    Update ReceiptController.php

--- app/Http/Controllers/ReceiptController.php
+++ app/Http/Controllers/ReceiptController.php
@@ -12,5 +12,32 @@ class ReceiptController extends Controller
         return view('home');
     }
 
-    
+    public function create(Request $request)
+    {
+
+        if ($request->file('image')) {
+            $image_path = $request->file('image')->store('public/itemImages');
+        }
+        else {
+            $image_path = "";
+        }
+
+        $receipt = new Receipt();
+
+        $receipt->service_type = $request->post('service_type');
+        $receipt->name = $request->post('name');
+        $receipt->contact_number = $request->post('contact_number');
+        $receipt->brand = $request->post('brand');
+        $receipt->warranty_card = $request->post('warranty_card');
+        $receipt->name = $request->post('name');
+        $receipt->image = $request->post('image');
+        $receipt->remark = $request->post('remark');
+        $receipt->cost = $request->post('cost');
+        $receipt->prepayment = $request->post('prepayment');
+        $receipt->status_id = $request->post('status_id');
+        dd($receipt);
+        $receipt->save();
+
+        return view('home');
+    }
 }
