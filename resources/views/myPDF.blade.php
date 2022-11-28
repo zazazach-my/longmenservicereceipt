7cb4554604e9c56fd0c1bd7c9210ddcb9224c3a6
zazazach-my <78088798+zazazach-my@users.noreply.github.com>
2022-11-29 01:22:35 +0800

    Update myPDF.blade.php

--- resources/views/myPDF.blade.php
+++ resources/views/myPDF.blade.php
@@ -32,6 +32,13 @@
     .text-right{
         text-align:right !important;
     }
+    .text-wrap{
+        white-space: pre-wrap;
+        white-space: -moz-pre-wrap;
+        white-space: -pre-wrap;
+        white-space: -o-pre-wrap;
+        word-wrap: break-word;
+    }
     .w-100{
         width: 100%;
     }
@@ -114,124 +121,96 @@
         background:#fff; 
         padding:0 10px; 
     }
+    .topleft {
+        vertical-align: top;
+        text-align: left;
+    }
+    .notice{
+        font-size:13px;
+    }
 </style>
 <body>
-<div class="w-100">
-    <div class="w-20 float-left ">
-        <div><h4 class="m-0">KLANG LONGMEN (M) SDN.BHD.</h4><h6 class="m-0 line" ><span class="line-span">(486132-M)</span></h6><h5 class="m-0">Shaw Centrepoint, G-21, Jalan Melayu,<br> 41400 Klang, Selangor, Malaysia.<br><i class="fa fa-whatsapp" style="font-size:24px"></i> Whatsapp: +6011-56480 693</h5></div>
+    <div class="w-100">
+        <div class="w-20 float-left ">
+            <div><h4 class="m-0">KLANG LONGMEN (M) SDN.BHD.</h4><h6 class="m-0 line" ><span class="line-span">(486132-M)</span></h6><h5 class="m-0">Shaw Centrepoint, G-21, Jalan Melayu,<br> 41400 Klang, Selangor, Malaysia.<br><i class="fa fa-whatsapp" style="font-size:24px"></i> Whatsapp: +6011 - 564 80693</h5></div>
+        </div>
+        <div class="w-80 float-left"></div>
     </div>
-    <div class="w-80 float-left"></div>
-</div>
-<div style="clear: both;"></div>
-<div class="add-detail mt-10">
-    <div class="w-30 float-left mt-10">
+    <div style="clear: both;"></div>
+    <div class="add-detail mt-10">
+        <div class="w-30 float-left mt-10">
+        </div>
+        <div class="w-40 float-left mt-10">
+            <h3 class="text-center m-0 p-0">修理单<br>SERVICE RECEIPT</h3>
+        </div>
+        <div class="w-30 float-left mt-10">
+            <p class="m-0 pt-5 text-right text-bold w-100">Receipt No - <span class="gray-color">202211-1</span></p>
+            <p class="m-0 pt-5 text-right text-bold w-100">Date - <span class="gray-color">31/12/2022</span></p>
+            <p class="m-0 pt-5 text-right text-bold w-100">Staff Name - <span class="gray-color">Kajib</span></p>
+        </div>
     </div>
-    <div class="w-40 float-left mt-10">
-        <h3 class="text-center m-0 p-0">修理单<br>SERVICE RECEIPT</h3>
+    <div style="clear: both;"></div>
+
+    <div class="table-section bill-tbl w-100 mt-10">
+        <table class="table w-100 mt-10">
+            <tr>
+                <td class="w-50">
+                    <div class="text-bold">Name 姓名:</div>
+                    <div class="text-center mt-10">Chong Teck Lam</div>
+                </td>
+                <td class="w-50">
+                    <div class="text-bold">Brand 牌子:</div>
+                    <div class="text-center mt-10">Seiko</div>
+                </td>
+            </tr>
+            <tr>
+                <td class="w-50">
+                    <div class="text-bold">Contact Number 联系号码:</div>
+                    <div class="text-center  mt-10">01133069690</div>
+                </td>
+                <td class="w-50">
+                    <div class="text-bold">Warranty Card 保证书：</div>
+                    <div class="text-center mt-10">No</div>
+                </td>
+            </tr>
+        </table>
     </div>
-    <div class="w-30 float-left mt-10">
-        <p class="m-0 pt-5 text-right text-bold w-100">Receipt No - <span class="gray-color">202211-1</span></p>
-        <p class="m-0 pt-5 text-right text-bold w-100">Date - <span class="gray-color">31/12/2022</span></p>
-        <p class="m-0 pt-5 text-right text-bold w-100"><br><span class="gray-color"></span></p>
+    <div class="table-section bill-tbl w-100 mt-10">
+        <table class="table w-100 mt-10">
+            <tr>
+                <td class="w-50 topleft" rowspan="2">
+                    <div class="text-bold">REMARKS:</div>
+                    <div class="text-center  mt-10">Images of the item are saved in the system<br> - Battery Replacement ( Not yet Paid ) <br>- Service ( TBC ) </h3></div>
+                </td>
+                <td class="w-50">
+                    <div class="total-part">
+                        <div class="total-left w-70 float-left" align="right">
+                            <p>Estimated Price</p>
+                            <p>Prepayment</p>
+                            <p>Balance</p>
+                        </div>
+                        <div class="total-right w-30 float-left text-bold" align="right">
+                            <p>TBC</p>
+                            <p>RM 20</p>
+                            <p>TBC</p>
+                        </div>
+                        <div style="clear: both;"></div>
+                    </div> 
+                </td>
+            </tr>
+        </table>
     </div>
-</div>
-<div class="table-section bill-tbl w-100 mt-10">
-    <table class="table w-100 mt-10">
-        <tr>
-            <td class="w-50">From</td>
-            <td class="w-50">To</td>
-        </tr>
-        <tr>
-            <td>
-                <div class="box-text">
-                    <p>Gujarat</p>
-                    <p>360004</p>
-                    <p>Near Haveli Road,</p>
-                    <p>Lal Darvaja,</p>
-                    <p>India</p>
-                    <p>Contact : 1234567890</p>
-                </div>
-            </td>
-            <td>
-                <div class="box-text">
-                    <p>Rajkot</p>
-                    <p>360012</p>
-                    <p>Hanumanji Temple,</p>
-                    <p>Lati Ploat</p>
-                    <p>Gujarat</p>
-                    <p>Contact : 1234567890</p>
-                </div>
-            </td>
-        </tr>
-    </table>
-</div>
-<div class="table-section bill-tbl w-100 mt-10">
-    <table class="table w-100 mt-10">
-        <tr>
-            <th class="w-50">Payment Method</th>
-            <th class="w-50">Shipping Method</th>
-        </tr>
-        <tr>
-            <td>Cash On Delivery</td>
-            <td>Free Shipping - Free Shipping</td>
-        </tr>
-    </table>
-</div>
-<div class="table-section bill-tbl w-100 mt-10">
-    <table class="table w-100 mt-10">
-        <tr>
-            <th class="w-50">SKU</th>
-            <th class="w-50">Product Name</th>
-            <th class="w-50">Price</th>
-            <th class="w-50">Qty</th>
-            <th class="w-50">Subtotal</th>
-            <th class="w-50">Tax Amount</th>
-            <th class="w-50">Grand Total</th>
-        </tr>
-        <tr align="center">
-            <td>$656</td>
-            <td>Mobile</td>
-            <td>$204.2</td>
-            <td>3</td>
-            <td>$500</td>
-            <td>$50</td>
-            <td>$100.60</td>
-        </tr>
-        <tr align="center">
-            <td>$656</td>
-            <td>Mobile</td>
-            <td>$254.2</td>
-            <td>2</td>
-            <td>$500</td>
-            <td>$50</td>
-            <td>$120.00</td>
-        </tr>
-        <tr align="center">
-            <td>$656</td>
-            <td>Mobile</td>
-            <td>$554.2</td>
-            <td>5</td>
-            <td>$500</td>
-            <td>$50</td>
-            <td>$100.00</td>
-        </tr>
-        <tr>
-            <td colspan="7">
-                <div class="total-part">
-                    <div class="total-left w-85 float-left" align="right">
-                        <p>Sub Total</p>
-                        <p>Tax (18%)</p>
-                        <p>Total Payable</p>
-                    </div>
-                    <div class="total-right w-15 float-left text-bold" align="right">
-                        <p>$20</p>
-                        <p>$20</p>
-                        <p>$330.00</p>
-                    </div>
-                    <div style="clear: both;"></div>
-                </div> 
-            </td>
-        </tr>
-    </table>
-</div>
+    <div>
+        <h4>CONDITIONS OF SERVICE</h4>
+        <div class="w-50 notice">
+            <li>Goods sold are not returnable or exchangable.</li>
+            <li>Repair Warranty - 90 days on the manufacture defects. Physical damage and Water damage are not included</li>
+            <li>Please inspect your goods immediately and inform us any issue that you notice upon the collection of your goods from our store. We do not take responsibility for any progression in damage for your goods that have been damaged by liquid and physical impact. Although unlikely, opening the your goods could set off further damage and in some cases may be very serious and irreparable.</li>
+            <li>We shall notify you when the Device has been repaired and is available for collection from store. If the device has not been collected it will be kept for future collection. If necessary, we shall send a reminder and the device will be recycled if not collected by day 60.
+        </div>
+    </div>
+    
+        Receipt was created on a computer and is valid without the signature.
+    
+</body>
 </html>
